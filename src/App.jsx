bdd9e64b6430f31281bef5a2cf95ff42c030a39a
kostaskatevas <katevaskostas@gmail.com>
2025-02-06 17:31:46 +0200

    added folder for navigation and installed redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
-import Home from './pages/Home';
-import SinglePost from './pages/SinglePost';
 import './App.css';
+import RouterNavigator from "./router/RouterNavigator.jsx";
 
-function App() {
+const App = () => {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <ThemeProvider>
-      <Router>
-        <div className="app">
-          <Routes>
-            <Route path="/" element={<Home isLoaded={isLoaded} setIsLoaded={setIsLoaded} />} />
-            <Route path="/post/:id" element={<SinglePost isLoaded={isLoaded} setIsLoaded={setIsLoaded} />} />
-          </Routes>
-        </div>
-      </Router>
+      <RouterNavigator isLoaded={isLoaded} setIsLoaded={setIsLoaded} />
     </ThemeProvider>
   );
 }
---
+++ src/router/RouterNavigator.jsx
@@ -0,0 +1,17 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "../pages/Home.jsx";
+import SinglePost from "../pages/SinglePost.jsx";
+
+// eslint-disable-next-line react/prop-types
+const RouterNavigator = ({isLoaded, setIsLoaded}) => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home isLoaded={isLoaded} setIsLoaded={setIsLoaded} />} />
+                <Route path="/post/:id" element={<SinglePost isLoaded={isLoaded} setIsLoaded={setIsLoaded} />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default RouterNavigator;
