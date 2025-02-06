e07818db8d8066ec7dfe44394d9fe311b99f1b2c
kostaskatevas <katevaskostas@gmail.com>
2025-02-06 15:43:54 +0200

    fixed flicking when loading the image

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ParticleBackground from '../components/shared/ParticleBackground';
