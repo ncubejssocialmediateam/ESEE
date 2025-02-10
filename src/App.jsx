3a5e98710caac4fd1ce01ea24bd9340853a07727
ncubejssocialmediateam <126887175+ncubejssocialmediateam@users.noreply.github.com>
2025-02-10 18:59:44 +0200

    Remove Amplify configuration and update App and News components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import Home from './pages/Home';
 import SinglePost from './pages/SinglePost';
+import Articles from './pages/admin/Articles';
+import AdminLayout from './components/layout/AdminLayout';
 import './App.css';
 
 function App() {
@@ -13,8 +15,14 @@ function App() {
       <Router>
         <div className="app">
           <Routes>
+            {/* Public Routes */}
             <Route path="/" element={<Home isLoaded={isLoaded} setIsLoaded={setIsLoaded} />} />
             <Route path="/post/:id" element={<SinglePost isLoaded={isLoaded} setIsLoaded={setIsLoaded} />} />
+            
+            {/* Admin Routes */}
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route path="articles" element={<Articles />} />
+            </Route>
           </Routes>
         </div>
       </Router>
